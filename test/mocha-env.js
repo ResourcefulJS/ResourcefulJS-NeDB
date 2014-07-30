305e96dde1bdf213dbc78bc4616e997a6bfc8ccd
Ricki Runge <ricki@ricki.dk>
2014-07-30 11:05:30 +0200

    Initial project files

---
+++ test/mocha-env.js
@@ -0,0 +1,12 @@
+global.chai = require("chai");
+global.assert = global.chai.assert;
+global.expect = global.chai.expect;
+global.should = global.chai.should();
+
+global.sinon = require("sinon");
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
