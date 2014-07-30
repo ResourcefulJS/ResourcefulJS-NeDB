b4c9b0a909679751c9c917e7e7770c6643c57361
Ricki Runge <ricki@ricki.dk>
2014-07-30 11:53:29 +0200

    Changed main to index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+
+module.exports = require("./lib/resourcefuljs-nedb.js");
---
+++ test/resourcefuljs-nedb.test.js
@@ -0,0 +1,6 @@
+var resourcefuljsNedb = require("../lib/resourcefuljs-nedb.js");
+
+describe("ResourcefulJS-NeDB", function() {
+
+});
+
